a664298ac66fd622b5a9b6ef7c19ef36cd2215ab
Luxurys-Lukuchi <146846830+Luxurys-Lukuchi@users.noreply.github.com>
2025-04-29 04:16:01 +0300

    Update calculation.dart

--- laba5/lib/models/calculation.dart
+++ laba5/lib/models/calculation.dart
@@ -1,1 +1,39 @@
+class Calculation {
+  final int? id;
+  final double capital;
+  final double term;
+  final double rate;
+  final double interest;
+  final DateTime date;
 
+  Calculation({
+    this.id,
+    required this.capital,
+    required this.term,
+    required this.rate,
+    required this.interest,
+    required this.date,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'capital': capital,
+      'term': term,
+      'rate': rate,
+      'interest': interest,
+      'date': date.toIso8601String(),
+    };
+  }
+
+  factory Calculation.fromMap(Map<String, dynamic> map) {
+    return Calculation(
+      id: map['id'],
+      capital: map['capital'],
+      term: map['term'],
+      rate: map['rate'],
+      interest: map['interest'],
+      date: DateTime.parse(map['date']),
+    );
+  }
+}
