27b526ac43d3835dfbc10701c34c2d46b410d708
Luxurys-Lukuchi <146846830+Luxurys-Lukuchi@users.noreply.github.com>
2025-05-03 04:36:47 +0300

    Create nasa_cubit.dart

---
+++ laba6/lib/cubit/nasa_cubit.dart
@@ -0,0 +1,30 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import '../requests/api.dart';
+import '../models/nasa_response.dart';
+
+abstract class NasaState {}
+
+class NasaLoading extends NasaState {}
+
+class NasaLoaded extends NasaState {
+  final NasaResponse data;
+  NasaLoaded(this.data);
+}
+
+class NasaError extends NasaState {
+  final String message;
+  NasaError(this.message); // Добавлен конструктор
+}
+
+class NasaCubit extends Cubit<NasaState> {
+  NasaCubit() : super(NasaLoading());
+
+  void fetchData() async {
+    try {
+      final data = await ApiService.getPhotos();
+      emit(NasaLoaded(data));
+    } catch (e) {
+      emit(NasaError('Ошибка: $e'));
+    }
+  }
+}
